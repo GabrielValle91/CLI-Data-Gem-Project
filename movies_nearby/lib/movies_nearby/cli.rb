39dc2c35d156a88cf48f23511405ae9b157c775a
Gabriel Valle <gvalle91@gmail.com>
2018-06-25 17:00:29 +0000

    added notes for which steps need to happen

--- movies_nearby/lib/movies_nearby/cli.rb
+++ movies_nearby/lib/movies_nearby/cli.rb
@@ -1,3 +1,11 @@
 class MoviesNearBy::CLI
 
+  def call
+    #greet the user and ask for a zip
+    #create movies found based on zip
+    #list Movies
+    #ask which movie to provide more detail on
+    #say goodbye to user upon exit
+  end
+
 end
