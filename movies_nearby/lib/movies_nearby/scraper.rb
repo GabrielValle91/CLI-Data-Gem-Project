b214ff9f7598a367e6010e330d3b8cfad1f9b96a
Gabriel Valle <gvalle91@gmail.com>
2018-06-25 17:53:54 +0000

    setup first scraper method with Nokogiri and ready to return array of movies

--- movies_nearby/lib/movies_nearby/scraper.rb
+++ movies_nearby/lib/movies_nearby/scraper.rb
@@ -1,5 +1,10 @@
 class MovieScraper
   def self.scrape_movie_overview(overview_url, zip_code)
+    html = open(overview_url)
+    doc = Nokogiri::HTML(html)
+    movie_array = []
 
+
+    movie_array
   end
 end
