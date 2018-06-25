f55769196b04fe92c14739368475afe5f62567ec
Gabriel Valle <gvalle91@gmail.com>
2018-06-25 17:52:05 +0000

    created class variable to store all movie objects and method to add movie details

--- movies_nearby/lib/movies_nearby/cli.rb
+++ movies_nearby/lib/movies_nearby/cli.rb
@@ -15,7 +15,13 @@ class MoviesNearBy::CLI
     overview_url = "https://www.imdb.com/showtimes/location/US/#{zip_code}".chomp
     movies = MovieScraper.scrape_movie_overview(overview_url, zip_code) #need to create a method to scrape a list of movies and urls from imdb
     MoviesNearBy::Movies.create_from_list(movies) #need to create a method to create multiple instances of Movie class from an array
-
+    #since the movie details are stored on a separate page, a different scraper method is needed
+    #the details will be stored as a hash and then added to the corresponding Movie instance
+    movie_details = {}
+    MoviesNearBy::Movies.all.each do |movie|
+      movie_details = MovieScraper.scrape_movie_details(movie.url)
+      movie.add_moviedetails(movie_details)
+    end
   end
 
 end
--- movies_nearby/lib/movies_nearby/movies.rb
+++ movies_nearby/lib/movies_nearby/movies.rb
@@ -1,5 +1,14 @@
 class MoviesNearBy::Movies
+  @@all = []
   def self.create_from_list(movies)
 
+  end
+
+  def add_moviedetails(movie_details)
+
+  end
+
+  def self.all
+    @@all
   end
 end
