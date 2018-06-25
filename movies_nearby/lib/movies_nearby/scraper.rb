bd970fbb69480600b1bcf3eaa8f87ba4f8a4cd7e
Gabriel Valle <gvalle91@gmail.com>
2018-06-25 18:01:06 +0000

    discovered attribute for title of each movie

--- movies_nearby/lib/movies_nearby/scraper.rb
+++ movies_nearby/lib/movies_nearby/scraper.rb
@@ -5,7 +5,14 @@ class MovieScraper
     movie_array = []
     binding.pry
     movie_list = doc.css(".lister-item")
+    #after finding the correct attribute for the list of movies, need to iterate through them to grab the correct details (name and url)
+    #these details will be storage in a hash and then pushed onto the movie_array
+    movie_list.each do |movie|
+      movie_hash = {}
+      movie_hash[:name] = movie.css(".title")
 
+      movie_array << movie_hash
+    end
 
     movie_array
   end
