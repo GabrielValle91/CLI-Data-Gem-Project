3b9a88b487f424ac4b1f956e5f71ebaef1c65fd0
Gabriel Valle <gvalle91@gmail.com>
2018-06-25 18:03:20 +0000

    discovered attribute for each movie url

--- movies_nearby/lib/movies_nearby/scraper.rb
+++ movies_nearby/lib/movies_nearby/scraper.rb
@@ -3,13 +3,15 @@ class MovieScraper
     html = open(overview_url)
     doc = Nokogiri::HTML(html)
     movie_array = []
-    binding.pry
+
     movie_list = doc.css(".lister-item")
     #after finding the correct attribute for the list of movies, need to iterate through them to grab the correct details (name and url)
     #these details will be storage in a hash and then pushed onto the movie_array
     movie_list.each do |movie|
       movie_hash = {}
       movie_hash[:name] = movie.css(".title")
+      movie_hash[:url] = movie.css("a").attribute("href").value
+      binding.pry
 
       movie_array << movie_hash
     end
